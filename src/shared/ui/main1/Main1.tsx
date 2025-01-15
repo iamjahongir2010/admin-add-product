afc6b349c114d10b57644cfc4d748c66a8028f57
iamjahongir2010 <i.jaha2010@gmail.com>
2025-01-15 17:51:39 +0500

    notSerioslyMain4

--- src/shared/ui/main1/Main1.tsx
+++ src/shared/ui/main1/Main1.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Main1() {
     return (
-        <div>Main1</div>
+        <div>
+            <li>
+                <ol>это адын</ol>
+                <ol>это дува</ol>
+                <ol>это тыри</ol>
+                <ol>это читыри</ol>
+            </li>
+        </div>
     )
 }
 
